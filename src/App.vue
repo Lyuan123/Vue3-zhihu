6c2698e119ddfed1f22083e647d317ca72012cde
Lyuan123 <1824581048@qq.com>
2022-03-03 16:24:53 +0800

    kjgh

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container" id="app">
+    <column-list :list="list"/>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList , { ColumnProps } from './components/ColumnList.vue'
+const testData: ColumnProps[] =[
+  {
+    id:1,
+    title:'asdas1的专栏',
+    description:'这是test1的专栏,是一个恒有与i四的简介',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg'
+  },
+    {
+    id:1,
+    title:'asdas2的专栏',
+    description:'这是test1的专栏,是一个恒有与i四的简介'
+    // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg'
+  },
+   {
+    id:1,
+    title:'asdas1的专栏',
+    description:'这是test1的专栏,是一个恒有与i四的简介',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg'
+  },
+    {
+    id:1,
+    title:'asdas2的专栏',
+    description:'这是test1的专栏,是一个恒有与i四的简介',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg'
+  },
+]
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    ColumnList
+  },
+  setup() {
+    return {
+      list: testData
+    }
   }
 })
 </script>
--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -1,20 +1,24 @@
 <template>
-  <ul>
-    <li v-for="column in list" :key="column.id">
-      <img :src="column.avatar" :alt="column.title">
-      <h5>{{column.title}}</h5>
-      <p>{{column.description}}</p>
-      <a href="#">进入专栏</a>
-    </li>
-  </ul>
+  <div class="row">
+    <div v-for="column in list" :key="column.id" class="col-4 mb-4">
+       <div class="card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img :src="column.avatar " :alt="column.title" class="rounded-circle border border-light w-25 my-3" >
+          <h6 class="card-title">{{column.title}}</h6>
+          <p class="card-text text-left">{{column.description}}</p>
+          <router-link :to="`/column/${column._id}`" class="btn btn-outline-primary">进入专栏</router-link>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-import {defineComponent, PropType} from 'vue'
+<script lang="ts">
+import {computed, defineComponent, PropType} from 'vue'
 export interface ColumnProps {
   id:number;
   title:String;
-  avatar: String;
+  avatar?: String;
   description: String;
 }
 export default defineComponent({
@@ -22,11 +26,18 @@ export default defineComponent({
   props: {
     list: {
       type: Array as PropType<ColumnProps[]>,
-      required:true
+      required: true
     }
   },
   setup(props){
-    
+    const columnList = computed(() =>{
+      return props.list.map(column =>{
+        if(!column.avatar){
+          column.avatar = require('../assets/column.jpg')
+        }
+        return column
+      })
+    })
   }
 })
 </script>
